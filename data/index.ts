80533a904c11500ec3a3bbfec0077b76d2fb0ee2
RoJack33 <145187320+RoJack33@users.noreply.github.com>
2024-09-07 15:20:18 +0900

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.avif",
+    img: "https://rojack33.github.io/jackson-portfolio/b1.avif",
     spareImg: "",
   },
   {
@@ -43,7 +43,7 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
+    img: "https://rojack33.github.io/jackson-portfolio/grid.svg",
     spareImg: "/b4.svg",
   },
 
@@ -54,8 +54,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: "https://rojack33.github.io/jackson-portfolio/b5.svg",
+    spareImg: "https://rojack33.github.io/jackson-portfolio/grid.svg",
   },
   {
     id: 6,
